d0c0700a4aa847db2f2d50d1331df63d991992b0
Rocko <codingwithrock@gmail.com>
2022-09-02 00:12:58 +0600

    all country university list

--- js/read-more.js
+++ js/read-more.js
@@ -1,9 +1,49 @@
 // Country Wise University. API
-const universityUrl = "http://universities.hipolabs.com/search?country=bangladesh";
 
 
 
-const universityList = (countryName)=> 
-{
-    console.log(countryName);
+callingAllUniversityList = async (countryName) => {
+    universityUrl = `http://universities.hipolabs.com/search?country=${countryName}`;
+
+    try {
+
+   
+        const response = await fetch(universityUrl);
+
+        const country = await response.json();
+
+        universityList(country);
+     
+
+    }
+    catch (error) {
+        console.log(error);
+    }
 }
+
+
+
+
+
+
+
+
+// !All University List of Specific Country Start
+const universityList = (country)=>
+{
+    console.log(country);
+    
+    
+    country.forEach(country => {
+
+        console.log(country.name);
+        console.log(country.web_pages[0]);
+       
+        
+    });
+    
+}
+// !All University List of Specific Country End
+
+
+
