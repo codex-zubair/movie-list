1709bfedb22b70ec5a3ffba8454e93bb8932eb78
Rocko <codingwithrock@gmail.com>
2022-09-01 23:53:22 +0600

    getting country Name

--- js/read-more.js
+++ js/read-more.js
@@ -1,2 +1,9 @@
 // Country Wise University. API
 const universityUrl = "http://universities.hipolabs.com/search?country=bangladesh";
+
+
+
+const universityList = (countryName)=> 
+{
+    console.log(countryName);
+}
